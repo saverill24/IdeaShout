e0d70cb5273dcaea972d164a052051f07d133c65
Sean Averill <saverill24@gmail.com>
2015-05-29 21:51:22 -0400

    stuff and new logo

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'products/tiles'
+  
 
   get 'welcome/homepage'
 
@@ -11,6 +11,8 @@ Rails.application.routes.draw do
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
+get 'products/tiles' => 'products#tiles'
+
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
