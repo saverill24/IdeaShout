1b13b4649ee05a730a9c7df2abc88241caf9b674
averilla13 <averilla13@gmail.com>
2015-05-30 19:58:27 -0400

    Products controller, with tiles and homepage connected

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
-  get 'products/tiles'
+  
 
-  get 'welcome/homepage'
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
    root 'welcome#homepage'
+  
+    get 'products/tiles'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
