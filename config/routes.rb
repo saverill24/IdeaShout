4d987c6255806fa4f1a89fbb8f82dff1fb5cd4b7
averilla13 <averilla13@gmail.com>
2015-05-31 11:16:15 -0400

    contact page setup and linked

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
 
 
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -14,6 +15,8 @@ Rails.application.routes.draw do
     get 'products/tiles'
 
 get 'descriptions/details'
+get 'contacts/contact'
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
